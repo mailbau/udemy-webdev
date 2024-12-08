3039230f88725c5fe58469c27a9912472fd0b19e
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2024-12-08 17:49:42 +0700

    feat: coding exercise 27

---
+++ Exercises n stuff/CodingExercise/CodingExercise27/index.js
@@ -0,0 +1,6 @@
+const planets = ['The Moon', 'Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
+planets.shift(); // Remove the first element, "The Moon"
+planets.push("Saturn"); // Add "Saturn" to the end of the array
+planets.unshift("Mercury"); // Add "Mercury" as the first element
