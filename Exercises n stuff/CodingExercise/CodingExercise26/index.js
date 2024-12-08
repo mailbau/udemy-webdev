9093976500cd5660c9a9f776322803a784bf920e
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2024-12-08 17:47:08 +0700

    feat: coding exercise 26

---
+++ Exercises n stuff/CodingExercise/CodingExercise26/index.js
@@ -0,0 +1,5 @@
+const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix']; //DON'T TOUCH THIS LINE!
+
+// YOUR CODE BELOW HERE:
+leaderboard[1] = 'Luna'; // Update the 2nd element (index 1) to "Luna"
+leaderboard[3] = 'Draco'; // Update the last element (index 3) to "Draco"
